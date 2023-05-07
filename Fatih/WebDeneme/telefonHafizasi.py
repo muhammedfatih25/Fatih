b8679490f40ec1118164935b374feb7dfede4684
muhammedfatih25 <muhammedhns@gmail.com>
2023-05-07 17:14:41 +0300

    MEsaj gönder

---
+++ Fatih/WebDeneme/telefonHafizasi.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+from gensim.models import Word2Vec
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+print('mmerhaba')
+
+df=pd.read_excel('Bap projesi.xlsx')
+df.head()
