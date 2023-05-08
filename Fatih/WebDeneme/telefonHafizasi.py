ac0b0c114f5783adb56f873a522817c7ae7a79ce
muhammedfatih25 <33536698+muhammedfatih25@users.noreply.github.com>
2023-05-08 06:51:53 +0300

    modified:   Fatih/WebDeneme/telefonHafizasi.py 	Fatih/Fatih/115.jpg 	Fatih/Fatih/CN_and_NN.h5 	Fatih/WebDeneme/telefonHafizasi.ipynb

--- Fatih/WebDeneme/telefonHafizasi.py
+++ Fatih/WebDeneme/telefonHafizasi.py
@@ -1,14 +1,43 @@
-
-import numpy as np
-from gensim.models import Word2Vec
-from sklearn.manifold import TSNE
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing import image
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
+import os
+# 
+# 
+# 
+import cv2 as cv
 
 
 
+def load_image(img_path, show=False):
 
-print('mmerhaba')
+    img = tf.keras.utils.load_img(img_path, target_size=(64, 64))
+    img_tensor = tf.keras.utils.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
 
-df=pd.read_excel('Bap projesi.xlsx')
-df.head()
+    return img_tensor
+
+    # load model
+model = load_model("Fatih\\CN_and_NN.h5")
+
+    # image path
+img_path = 'Fatih\\Fatih\\115.jpg'   
+# dosyaYolu="Dataset/deneme2"
+# files=os.listdir(dosyaYolu)
+#for f in files:
+    # load a single image
+new_image = load_image(img_path)
+print(new_image)
+
+        # check prediction
+pred = model.predict(new_image)
+print(pred)
+
+if pred[0][0] >= 0.5:
+    prediction = 'normal'
+else:
+    prediction = 'cataract'
+print(prediction)
